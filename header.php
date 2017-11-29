9287876a8eb01faec786509428bb044d57488f27
Gago111 <gago2009@live.nl>
2017-11-29 14:55:54 +0100

    Loginsystem

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+ ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title></title>
+</head>
+<body>
+
+   <header>
+        <nav>
+            <div class="main-wrapper">
+                <ul>
+                   <li><a href="index.php">Home</a></li>
+                 </ul>
+                 <div class="nav-login">
+                   <form action="includes/login.inc.php" method="POST">
+                     <input type="text" name="uid" placeholder="Username/e-mail">
+                     <input type="password" name="pwd" placeholder="Password">
+                     <button type="submit" name="submit">Login</button>
+                   </form>
+                   <a href="signup.php">Sign up</a>
+             </div>
+          </nav>
+   </header>
---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "admin77";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName );
